30684c52d811c97759c770b7586fdfaaac17e89c
uilianzimer <uilianzimer333@gmail.com>
2024-04-10 11:16:58 -0300

    banco-de-dados

---
+++ SRC/routes.js
@@ -0,0 +1,10 @@
+
+import { Router } from "express"
+
+const routes = new Router()
+
+routes.get("/", (request, response) => {
+    return response.json({ message: "Hello World" })
+})
+
+export default routes
